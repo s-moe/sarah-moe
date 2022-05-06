c7e37f4612a8ff95d8cebac09acc8887ddc09f7b
Sarah Moe <sarahannmoe@gmail.com>
2022-05-06 15:58:23 -0700

    created animation for hamburger menu

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Skills from "./components/skills/Skills";
 import Works from "./components/works/Works";
 import "./app.scss";
+import { useState } from "react";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(true);
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />

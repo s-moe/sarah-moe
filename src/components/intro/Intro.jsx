5fb3d542ab554b1f0c67c1ff8ca7ae8b738e328b
Sarah Moe <sarahannmoe@gmail.com>
2022-05-07 09:01:04 -0700

    edited img and topbar

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,14 @@
 import "./intro.scss";
 
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left"></div>
+      <div className="right">
+        <div className="imageContainer">
+          <img src="assets/profilePicture.jpg" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }

fe089fd22c87aa5e63d4f791974be7b50d810db4
Sarah Moe <sarahannmoe@gmail.com>
2022-05-07 12:20:18 -0700

    styling to portfolio page items and container

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,12 +5,30 @@ export default function Portfolio() {
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
-        <li>Featured</li>
-        <li>Featured</li>
-        <li>Featured</li>
-        <li>Featured</li>
-        <li>Featured</li>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
       </ul>
+      <div className="container">
+        <div className="item">
+          <img src="./assets/microsoftEditor.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="./assets/microsoftEditor.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="./assets/microsoftEditor.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="./assets/microsoftEditor.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
   );
 }

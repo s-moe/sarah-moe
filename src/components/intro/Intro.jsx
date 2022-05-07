83eca66486627da440d2a1e573cbe455dbf07a56
Sarah Moe <sarahannmoe@gmail.com>
2022-05-07 10:21:02 -0700

    adding content to intro slide

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,13 +1,25 @@
 import "./intro.scss";
+import { ExpandMore } from "@material-ui/icons";
 
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-      <div className="left"></div>
-      <div className="right">
-        <div className="imageContainer">
+      <div className="left">
+        <div className="imgContainer">
           <img src="assets/profilePicture.jpg" alt="" />
         </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Sarah Moe</h1>
+          <h3>
+            Freelance <span>Designer</span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <ExpandMore id="arrowDown" />
+        </a>
       </div>
     </div>
   );

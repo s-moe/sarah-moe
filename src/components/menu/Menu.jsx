c08339980aeaf6424c02c4957639a4bce8daed49
Sarah Moe <sarahannmoe@gmail.com>
2022-05-06 17:03:53 -0700

    close menu when clicking item

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -4,23 +4,23 @@ export default function Menu({ menuOpen, setMenuOpen }) {
   return (
     <div className={"menu " + (menuOpen && "active")}>
       <ul>
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <a href="#intro">Home</a>
         </li>
 
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <a href="#portfolio">Portfolio</a>
         </li>
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <a href="#skills">Skills</a>
         </li>
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <a href="#works">Work</a>
         </li>
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <a href="#contact">Contact</a>
         </li>
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <a href="#blog">Blog</a>
         </li>
       </ul>

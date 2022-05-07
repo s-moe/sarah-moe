e498ef1a22374b4156b565201c2e2e9b2148a5a1
Sarah Moe <sarahannmoe@gmail.com>
2022-05-07 13:11:53 -0700

    removed list from portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,13 +4,7 @@ export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
-      <ul>
-        <li className="active">Featured</li>
-        <li>Web App</li>
-        <li>Mobile App</li>
-        <li>Design</li>
-        <li>Branding</li>
-      </ul>
+
       <div className="container">
         <div className="item">
           <img src="./assets/microsoftEditor.png" alt="" />

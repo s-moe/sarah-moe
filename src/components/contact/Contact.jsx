e3e0d1f4db8766d51187d1e4a037172a74f0b85a
Sarah Moe <sarahannmoe@gmail.com>
2022-05-08 10:02:26 -0700

    styling form and started media query for mobile responsiveness

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,14 +1,22 @@
 import "./contact.scss";
+import { useState } from "react";
 
 export default function Contact() {
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
   return (
     <div className="contact" id="contact">
       <div className="left">
         <h2>Contact</h2>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Email" />
           <textarea placeholder="Message"></textarea>
           <button type="submit">Send</button>
+          {message && <span>Thanks! I will reply ASAP</span>}
         </form>
       </div>
       <div className="right">

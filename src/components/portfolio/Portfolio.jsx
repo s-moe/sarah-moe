8714cdd4bce92565b6e75edca38821b5f63418c5
Sarah Moe <sarahannmoe@gmail.com>
2022-05-08 11:03:32 -0700

    created buttons and removed icons from portfolio cards

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,43 +4,43 @@ export default function Portfolio() {
   const data = [
     {
       id: 1,
-      name: "Tom Durden",
-      title: "Senior Developer",
-      img: "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      icon: "assets/twitter.png",
+      solidButton: "Live Site",
+      outlineButton: "Github",
+      img: "./assets/microsoftEditor.png",
+
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
     },
     {
       id: 2,
-      name: "Alex Kalinski",
-      title: "Co-Founder of DELKA",
+      solidButton: "Live Site",
+      outlineButton: "Github",
       img: "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      icon: "assets/youtube.png",
+
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
     },
     {
       id: 3,
-      name: "Martin Harold",
-      title: "CEO of ALBI",
+      solidButton: "Live Site",
+      outlineButton: "Github",
       img: "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      icon: "assets/linkedin.png",
+
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
       featured: true,
     },
     {
       id: 4,
-      name: "Martin Harold",
-      title: "CEO of ALBI",
+      solidButton: "Live Site",
+      outlineButton: "Github",
       img: "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      icon: "assets/linkedin.png",
+
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
     },
     {
       id: 5,
-      name: "Martin Harold",
-      title: "CEO of ALBI",
+      solidButton: "Live Site",
+      outlineButton: "Github",
       img: "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      icon: "assets/linkedin.png",
+
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
     },
   ];
@@ -58,8 +58,8 @@ export default function Portfolio() {
             </div>
             <div className="center">{d.desc}</div>
             <div className="bottom">
-              <h3>{d.name}</h3>
-              <h4>{d.title}</h4>
+              <button>{d.solidButton}</button>
+              <button>{d.outlineButton}</button>
             </div>
           </div>
         ))}

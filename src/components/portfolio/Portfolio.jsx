22223bdd3c58cb360c232bd8cb58c69b603ba385
Sarah Moe <sarahannmoe@gmail.com>
2022-05-08 10:55:43 -0700

    revised portfolio page to include format and styling of card layout

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,33 +1,68 @@
 import "./portfolio.scss";
 
 export default function Portfolio() {
+  const data = [
+    {
+      id: 1,
+      name: "Tom Durden",
+      title: "Senior Developer",
+      img: "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/twitter.png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
+    },
+    {
+      id: 2,
+      name: "Alex Kalinski",
+      title: "Co-Founder of DELKA",
+      img: "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/youtube.png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
+    },
+    {
+      id: 3,
+      name: "Martin Harold",
+      title: "CEO of ALBI",
+      img: "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/linkedin.png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+      featured: true,
+    },
+    {
+      id: 4,
+      name: "Martin Harold",
+      title: "CEO of ALBI",
+      img: "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/linkedin.png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+    },
+    {
+      id: 5,
+      name: "Martin Harold",
+      title: "CEO of ALBI",
+      img: "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/linkedin.png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+    },
+  ];
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
 
       <div className="container">
-        <div className="item">
-          <img src="./assets/microsoftEditor.png" alt="" />
-          <h3>Description & Skills used</h3>
-          {/* <div className="imgButton">
-            <button className="github">Github</button>
+        {data.map((d) => (
+          <div className={d.featured ? "card featured" : "card"}>
+            <div className="top">
+              <img src="assets/right-arrow.png" className="left" alt="" />
+              <img className="user" src={d.img} alt="" />
+              <img className="right" src={d.icon} alt="" />
+            </div>
+            <div className="center">{d.desc}</div>
+            <div className="bottom">
+              <h3>{d.name}</h3>
+              <h4>{d.title}</h4>
+            </div>
           </div>
-          <div className="imgButton">
-            <button className="liveSite">Live Site</button>
-          </div> */}
-        </div>
-        <div className="item">
-          <img src="./assets/microsoftEditor.png" alt="" />
-          <h3>Description & Skills used</h3>
-        </div>
-        <div className="item">
-          <img src="./assets/microsoftEditor.png" alt="" />
-          <h3>Description & Skills used</h3>
-        </div>
-        <div className="item">
-          <img src="./assets/microsoftEditor.png" alt="" />
-          <h3>Description & Skills used</h3>
-        </div>
+        ))}
       </div>
     </div>
   );

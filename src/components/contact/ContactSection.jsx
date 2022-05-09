ba4f94bed56434ddeb09a68a87b22aa4ba5f99e3
Sarah Moe <sarahannmoe@gmail.com>
2022-05-09 16:33:51 -0700

    styled form component

--- src/components/contact/ContactSection.jsx
+++ src/components/contact/ContactSection.jsx
@@ -6,22 +6,23 @@ import Form from "../form/Form";
 import PhoneNumber from "../contactInfoItems/PhoneNumber";
 import EmailAddress from "../contactInfoItems/EmailAddress";
 import MyLocation from "../contactInfoItems/MyLocation";
-
+import "./contactSection.scss";
 import React from "react";
 
 export default function ContactSection() {
   return (
-    <div className="container">
-      <div className="contactSection__wrapper">
-        <div className="left">
-          <Form />
-        </div>
-        <div className="right">
-          <PhoneNumber />
-          <EmailAddress />
-          <MyLocation />
-        </div>
+    // <div className="container">
+    <div className="contactSection__wrapper">
+      <div className="left">
+        <Form />
+      </div>
+      <div className="right">
+        <h1>Contact Me</h1>
+        <PhoneNumber />
+        <EmailAddress />
+        <MyLocation />
       </div>
     </div>
+    // </div>
   );
 }
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,5 +1,6 @@
 import "./form.scss";
 import { useState } from "react";
+import emailjs from "emailjs-com";
 
 import React from "react";
 
@@ -8,58 +9,72 @@ export default function Form() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_9azi0y5",
+        "contact_form",
+        e.target,
+        "user_G82CO16402WG1xtpLLb2i"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    alert("Thank you for your message. I'll get back to you soon.");
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
   return (
-    <div>
-      <form className="form">
+    <>
+      <form className="form" onSubmit={handleSubmit}>
         <div className="form-group">
-          <label htmlFor="name">
-            Name
-            <input
-              type="text"
-              name="name"
-              placeholder="Name"
-              className="input"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            className="input"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
         <div className="form-group">
-          <label htmlFor="Email">
-            Email
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              className="input"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="input"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
 
         <div className="form-group">
-          <label htmlFor="message">
-            Message
-            <textarea
-              type="text"
-              name="message"
-              placeholder="message"
-              className="input"
-              id="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-          </label>
+          <textarea
+            type="text"
+            name="message"
+            placeholder="Message"
+            className="input"
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
         </div>
 
         <button type="submit" className="button">
           Send
         </button>
       </form>
-    </div>
+    </>
   );
 }

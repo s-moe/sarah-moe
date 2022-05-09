d8d14d6db3a8bf7d2d29fb2392f45c686277f069
Sarah Moe <sarahannmoe@gmail.com>
2022-05-09 14:18:53 -0700

    styling form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -9,55 +9,57 @@ export default function Form() {
   const [message, setMessage] = useState("");
 
   return (
-    <form>
-      <div className="form-group">
-        <label htmlFor="name">
-          Name
-          <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            className="input"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-      </div>
+    <div>
+      <form className="form">
+        <div className="form-group">
+          <label htmlFor="name">
+            Name
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              className="input"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+        </div>
 
-      <div className="form-group">
-        <label htmlFor="Email">
-          Email
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            className="input"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-      </div>
+        <div className="form-group">
+          <label htmlFor="Email">
+            Email
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className="input"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+        </div>
 
-      <div className="form-group">
-        <label htmlFor="message">
-          Message
-          <textarea
-            type="text"
-            name="message"
-            placeholder="message"
-            className="input"
-            id="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-        </label>
-      </div>
+        <div className="form-group">
+          <label htmlFor="message">
+            Message
+            <textarea
+              type="text"
+              name="message"
+              placeholder="message"
+              className="input"
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </label>
+        </div>
 
-      <button type="submit" className="button">
-        Send
-      </button>
-    </form>
+        <button type="submit" className="button">
+          Send
+        </button>
+      </form>
+    </div>
   );
 }
